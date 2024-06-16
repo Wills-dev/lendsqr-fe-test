79502c6214077f8bd07b8f3b9ba2b4bc26704b42
wills-dev <ejioguvictor6@gmail.com>
2024-06-16 11:29:43 +0100

    integrated test and logic for login

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,10 +1,16 @@
+import axios from "axios";
+
 // get user data
 
-"https://run.mocky.io/v3/7a81bb9c-d898-41ab-bbeb-8d750ef99719";
+("https://run.mocky.io/v3/7a81bb9c-d898-41ab-bbeb-8d750ef99719");
 
 //get user details
 
-"https://run.mocky.io/v3/24c0d8d5-1a7b-4e18-9540-6a3d46ca3299";
+("https://run.mocky.io/v3/24c0d8d5-1a7b-4e18-9540-6a3d46ca3299");
 
 //login
-"https://run.mocky.io/v3/9f91ddd6-6539-4a10-90ae-83d0c39cdf4a";
+("https://run.mocky.io/v3/62ae12da-f06b-4831-8eea-1e98e94ac6b5");
+
+export const axiosInstance = axios.create({
+  baseURL: "https://run.mocky.io/v3",
+});
