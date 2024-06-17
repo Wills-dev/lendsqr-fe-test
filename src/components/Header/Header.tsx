db1f0c27fe1d69d2efa4e1d45df12df5bde2f75a
wills-dev <ejioguvictor6@gmail.com>
2024-06-17 13:02:45 +0100

    feat: made sidebar responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="header__left__container">
         <div className="header__logo__wrapper">
           <img src="/logo.svg" alt="logo" />
-          {!isSideBarActive ? (
+          {!isSideBarActive && (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -23,22 +23,6 @@ const Header = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className=""
-              onClick={() => setIsSideBarActive(false)}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18 18 6M6 6l12 12"
               />
             </svg>
           )}
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -9,6 +9,26 @@ const Sidebar = () => {
   return (
     <div className={`side__container  ${isSideBarActive && "activateMenu"}`}>
       <div className="inner__container">
+        <div className="header__logo__wrapper">
+          <img src="/logo.svg" alt="logo" />
+          {isSideBarActive && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className=""
+              onClick={() => setIsSideBarActive(false)}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          )}
+        </div>
         <div className="dashboard__organization__content">
           <img src="/assets/icons/briefcase 1.svg" alt="brief-case" />
           <h6>Switch Organization</h6>
